215ebbce4e91fae7c82e2394de684170e7cc9759
mneum4nn <n3um4nn@proton.me>
2023-10-09 20:29:53 -0600

    Correct row variable to column

--- functions.js
+++ functions.js
@@ -1,10 +1,22 @@
-function createSquareGrid (n){
-    for (let i = 0; i < n; i++){
-        for (let j=0; j<n;j++){
-            $('.container').append("<div claass='grid'></div>")
-        }
-    }
-    $('.grid').width(960/n);
-    $('.grid').height(960/n);
 
+function createSquareGrid (n){
+    let container = document.querySelector('.container');
+
+    let grid = document.createElement('div');
+    grid.className='grid';
+
+    for (let i = 0; i < n; i++){
+        let column = document.createElement('div');
+        column.className='column';
+        for (let j=0; j < n;j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            column.appendChild(cell);
+        }
+        grid.appendChild(column);
+    }
+    container.appendChild(grid);
 }
+document.addEventListener('DOMContentLoaded', function() {
+    createSquareGrid(16);
+});
