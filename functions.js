8a9ecfd4b4ffa581a81ea758fefe521bc308e912
mneum4nn <n3um4nn@proton.me>
2023-10-11 12:42:02 -0600

    Create function to make custom grid and an event listener for the new grid button

--- functions.js
+++ functions.js
@@ -1,6 +1,8 @@
 let customSize = document.querySelector('.scale');
 let container = document.querySelector('.container');
 let grid = document.querySelector('.grid');
+let newgrid = document.querySelector('.newgrid');
+
 let defaultSize = 16;    
 
 createSquareGrid(defaultSize);
@@ -23,3 +25,26 @@ function createSquareGrid (scale){
     }
 }
 
+//TODO: custom grid function that removes the default 16x16 one and takes input as new dimensions
+function makeCustomGrid(){
+    //user input size
+    let newScale = parseInt(customSize.value);
+    //mak sure there is a size and that it fits our parameters
+    if (newScale != null && newScale <=100 && newScale>=2){
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        createSquareGrid(newScale);        
+    }
+    else if (newScale>100){
+        alert("Sorry! 100 x 100 is as big as we can go!");
+    } else if (newScale<2){
+        alert("Sorry! 2x2 is as small as we can go!");
+    } else if (newScale == null){
+        alert("Please input your desired square dimension");
+    }
+}
+//TODO: create event listener for new grid button
+newgrid.addEventListener('click', makeCustomGrid);
+
+//TODO: create event listener for hover event to change cell colors
